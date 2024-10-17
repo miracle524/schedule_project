0a51468eb71e917e4d2c0e2cb94c5ab871b2e3f0
miracle524 <miracle97524@outlook.com>
2024-10-17 07:38:40 +0200

    'Fixed

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/', include('schedule_app.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
